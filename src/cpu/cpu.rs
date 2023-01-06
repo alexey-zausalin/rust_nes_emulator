1a0de1f1ed3feece34253c37ee51b6ea140e9aaa
Alexey Zausalin <alexeyzausalin@gmail.com>
2023-01-06 11:22:11 +0200

    add test snake game

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -86,11 +86,21 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
+        self.run_with_callback(|_| {});
+    }
+    pub fn run_with_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU),
+    {
         let ref opcodes: HashMap<u8, &'static opscode::OpsCode> = *opscode::OPSCODES_MAP;
 
         loop {
+            callback(self);
+
             let code = self.mem_read(self.program_counter);
-            let opcode = opcodes.get(&code).unwrap();
+            let opcode = opcodes
+                .get(&code)
+                .expect(&format!("OpCode {:x} is not recognized", code));
 
             self.program_counter += 1;
             let program_counter_state = self.program_counter;
