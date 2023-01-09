ec5223c3f69e074df799a14042997e780e6c73d3
Alexey Zausalin <alexeyzausalin@gmail.com>
2023-01-09 11:35:23 +0200

    add ROM

--- src/nes/mod.rs
+++ src/nes/mod.rs
@@ -1,1 +1,2 @@
 pub mod ram;
+pub mod rom;
---
+++ src/nes/rom/mod.rs
@@ -0,0 +1,17 @@
+pub struct Rom {
+    data: Vec<u8>,
+}
+
+impl Rom {
+    pub fn new(data: Vec<u8>) -> Rom {
+        Rom { data }
+    }
+
+    pub fn read(&self, pos: u16) -> u8 {
+        self.data[pos as usize]
+    }
+
+    pub fn size(&self) -> usize {
+        self.data.len()
+    }
+}
